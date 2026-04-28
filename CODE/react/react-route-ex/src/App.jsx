e2f022c0c9fc5d528ca5cb31e3880440ee720725
Thushanika <thushanikaparamalingam02@gmail.com>
2026-04-28 15:20:26 +0530

    add route

--- CODE/react/react-route-ex/src/App.jsx
+++ CODE/react/react-route-ex/src/App.jsx
@@ -1,20 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
-
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
     <>
-     <div className="main-container">
-      <Header />
-      <About />
-      <Contact />
-      <Footer />
-     </div>
-     
+    <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/about" element={<About/>}/>
+      <Route path="/contact" element={<Contact/>}/>
+      <Route path="*" element={<NotFound />}/>
+    </Routes>
+    <Footer />
+    </BrowserRouter>
     </>
   );
 }
--- CODE/react/react-route-ex/src/components/Header.jsx
+++ CODE/react/react-route-ex/src/components/Header.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <>
-      <h1>Header</h1>
+      <ul>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/contact">Contact</Link>
+        </li>
+      </ul>
     </>
   );
 }
